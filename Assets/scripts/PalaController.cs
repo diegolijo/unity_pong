6917c6c4a1c11d8da9e64ae8b782d39eff5cd9ea
diegolijo <diegolijo@gmail.com>
2024-11-14 16:26:12 +0100

    pala script: speed, max/min,  deltaTime

--- Assets/scripts/PalaController.cs
+++ Assets/scripts/PalaController.cs
@@ -1,9 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    [SerializeField] float MAX_Y = 4.4f;
+    [SerializeField] float MIN_Y = -4.4f;
+    [SerializeField] float speed = 20.0f;
     void Start()
     {
 
@@ -13,14 +15,21 @@ public class NewBehaviourScript : MonoBehaviour
     {
         if (Input.GetKey("up"))
         {
-            //Stransform.Translate(new Vector3(0, .1f, 0));
-            transform.Translate(Vector3.up * .1f);
-        };
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
 
         if (Input.GetKey("down"))
         {
-            //transform.Translate(new Vector3(0, -.1f, 0));
-            transform.Translate(Vector3.up * -.1f);
-        };
+            transform.Translate(Vector3.up * speed * -Time.deltaTime);
+        }
+        if (transform.position.y > MAX_Y)
+        {
+            transform.position = new Vector3(8, MAX_Y, 0);
+        }
+
+        if (transform.position.y < MIN_Y)
+        {
+            transform.position = new Vector3(8, MIN_Y, 0);
+        }
     }
 }
